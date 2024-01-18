d008139b63c987f270ecffe1f901ebd5c2b752e0
nibormik <krjordanger@gmail.com>
2024-01-18 06:01:54 +0100

    tested running old alg without updating display still slow

--- WFC_TEST/Pygame visulize.py
+++ WFC_TEST/Pygame visulize.py
@@ -6,16 +6,12 @@ import time
 
 from WFC import *
 
-pygame.init()
 
-SIZE = (32,64)
+SIZE = (32,32)
 CELL_SET = "./Cells_image.json"
 CELL_SIZE = 15
 DISPLAY_SIZE = (SIZE[1]*CELL_SIZE,SIZE[0]*CELL_SIZE)
 flags= []
-DISPLAY = pygame.display.set_mode(DISPLAY_SIZE,pygame.SCALED)
-DRAW_SURFACE = pygame.display.set_mode(DISPLAY_SIZE,pygame.SCALED)
-DRAW_SURFACE = pygame.transform.rotate(DRAW_SURFACE,90)
 DONE = False
 ITER = 0
 SAVED = False
@@ -47,34 +43,14 @@ def draw_image(pos_X,pos_Y,size):
 
 
 tot_time = time.monotonic()
-while True:
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
+while not DONE:
+    ITER += 1
+    cells_left = 0
+    Tick_speed = time.monotonic()
+    DONE = WFC.tick()
+    Tick_speed = time.monotonic() - Tick_speed
 
-    if not DONE:
-        ITER += 1
-        cells_left = 0
-        Tick_speed = time.monotonic()
-        DONE = WFC.tick()
-        Tick_speed = time.monotonic() - Tick_speed
-
-
-    else:
-        if not SAVED:
-            pygame.image.save(DISPLAY,'Output.png')
     if time.monotonic() - draw > 1:
-
-        for x,v in enumerate(WFC.cell_grid):
-            for y,v in enumerate(v):
-                if v.Cell == None:
-                    cells_left += 1
-                draw_image(x,y,CELL_SIZE)
-
-
-        DISPLAY.blit(pygame.transform.rotate(DRAW_SURFACE,-90),(0,0))
-        pygame.display.update()
         draw = time.monotonic()
 
         it_psec = ITER - iterate
@@ -84,3 +60,24 @@ while True:
         print("Collapsing Cells")
         print(f"Iterations: {ITER}, Iterations p/s: {it_psec} ,Iteration time: {Tick_speed}")
         print(f"Cells left: {cells_left}, Tot time {time.monotonic()-tot_time}")
+
+pygame.init()
+DISPLAY = pygame.display.set_mode(DISPLAY_SIZE,pygame.SCALED)
+DRAW_SURFACE = pygame.display.set_mode(DISPLAY_SIZE,pygame.SCALED)
+DRAW_SURFACE = pygame.transform.rotate(DRAW_SURFACE,90)
+        
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+    for x,v in enumerate(WFC.cell_grid):
+            for y,v in enumerate(v):
+                if v.Cell == None:
+                    cells_left += 1
+                draw_image(x,y,CELL_SIZE)
+    DISPLAY.blit(pygame.transform.rotate(DRAW_SURFACE,-90),(0,0))
+    pygame.display.update()
+    if not SAVED:
+        pygame.image.save(DISPLAY,'Output.png')
+        SAVED = True
